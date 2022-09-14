5874dd4a9ef362c86cb2932956ad4eaf244113a0
iramsayyed <siram.gulab@gmail.com>
2022-09-14 18:10:34 +0530

    API JWT

---
+++ account/models.py
@@ -0,0 +1,81 @@
+from pyexpat import model
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+# Create your models here.
+# custom user manager
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, name, tc, password=None, password2=None):
+        """
+        Creates and saves a User with the given email, name, tc, password, password2
+        """
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            tc=tc,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, tc, password=None, ):
+        """
+        Creates and saves a superuser with the given email, name, tc
+        password and password2.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+            tc=tc,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+
+
+
+# custom user
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=200)
+    tc = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','tc']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
